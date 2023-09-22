2f337cfaee25df759813e99349dcc5fcd3990b77
Jason Hasselkus <jason.hasselkus@gmail.com>
2023-09-22 07:44:45 -0500

    Updates to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,35 @@
+import Link from 'next/link'
+
 const Navbar = () => {
 
     return (
-        <nav>
-            <h2>Navbar goes here</h2>
-        </nav>
+        <header className="bg-white">
+            <nav>
+                <div>
+                    <h2 className="font-bold text-2xl">static blog</h2>
+                </div>
+                <div className="">
+                    <ul className="">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <button>Sign In</button>
+                </div>
+            </nav>
+        </header>
+
     )
 }
 
